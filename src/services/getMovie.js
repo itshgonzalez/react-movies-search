444e6195bfc761027ea5403c65ce633f73e7d11d
Héctor González <itshgonzalez@gmail.com>
2020-06-24 01:39:56 +0200

    :sparkles: Update services get only movies

--- src/services/getMovie.js
+++ src/services/getMovie.js
@@ -1,4 +1,4 @@
 export async function getMovie(inputKeywords) {
-  const res = await fetch(`http://www.omdbapi.com/?apikey=${process.env.REACT_APP_APIKEY}&i=${inputKeywords.trim()}`);
+  const res = await fetch(`http://www.omdbapi.com/?apikey=${process.env.REACT_APP_APIKEY}&i=${inputKeywords}`);
   return await res.json();
-};
+}
--- src/services/getMovies.js
+++ src/services/getMovies.js
@@ -1,4 +1,4 @@
 export async function getMovies(inputKeywords) {
-  const res = await fetch(`http://www.omdbapi.com/?apikey=${process.env.REACT_APP_APIKEY}&s=${inputKeywords.trim()}`);
+  const res = await fetch(`http://www.omdbapi.com/?apikey=${process.env.REACT_APP_APIKEY}&s=${inputKeywords}&type=movie`);
   return await res.json();
-};
+}
