cde065761f39743d1d9ed9761b38c7ce908f5808
Héctor González <itshgonzalez@gmail.com>
2020-07-16 15:02:56 +0200

    :white_check_mark: Added tests with Jest

---
+++ src/__tests__/_app/_app.spec.js
@@ -0,0 +1,12 @@
+import React from "react";
+import renderer from "react-test-renderer";
+
+import App from "App";
+
+describe("App", () => {
+  it("render without errors", () => {
+    const wrapper = renderer.create(<App />);
+
+    expect(wrapper).toMatchSnapshot();
+  });
+});
