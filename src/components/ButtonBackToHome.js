3f566688a56a462a17410c060e7813006ad41dbf
Héctor González <itshgonzalez@gmail.com>
2020-07-18 19:16:48 +0200

    :art: Add styles to not found page

--- src/components/ButtonBackToHome.js
+++ src/components/ButtonBackToHome.js
@@ -1,6 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const ButtonBackToHome = () => <Link to={"/"}>Volver</Link>;
+import styled from "styled-components";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const Icon = styled(FontAwesomeIcon)`
+  width: 48px;
+  height: 48px;
+  transition: 0.2s ease-in-out;
+
+  &:hover {
+    transform: scale(1.2);
+  }
+`;
+
+const ButtonBackToHome = () => (
+  <Link to={"/"}>
+    <Icon icon={["fas", "arrow-left"]} size="2x" color="#000" />
+  </Link>
+);
 
 export default ButtonBackToHome;
--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,21 +1,43 @@
 import React from "react";
 
+import styled from "styled-components";
+
 import ButtonBackToHome from "components/ButtonBackToHome";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 60%;
+  height: 100%;
+  margin: 2rem auto;
+`;
+
+const NotFoundContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-size: 2.5rem;
+  margin: 1rem auto;
+`;
+
+const Title = styled.h1`
+  font-weight: 700;
+`;
+const SubTitle = styled.h2`
+  font-weight: 700;
+`;
 
 function NotFound() {
   return (
-    <div className="page-notfound">
-      <div className="left-notfound">
-        <ButtonBackToHome />
-      </div>
-      <div className="right-notfound">
-        <span role="img" aria-label="Sadface">
+    <Wrapper>
+      <ButtonBackToHome />
+      <NotFoundContent>
+        <span role="img" aria-label="sadface">
           😔
         </span>
-        <h1 className="title">404!</h1>
-        <h2 className="subtitle">Not found</h2>
-      </div>
-    </div>
+        <Title>404!</Title>
+        <SubTitle>Not found</SubTitle>
+      </NotFoundContent>
+    </Wrapper>
   );
 }
 
