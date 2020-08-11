041d509d29fbdd90bf675b3fd7d95babef61f6f0
Héctor González <itshgonzalez@gmail.com>
2020-08-11 14:54:15 +0200

    :recycle: Refactor routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import Detail from "pages/Detail";
-import Home from "pages/Home";
-import NotFound from "pages/NotFound";
-
 import { MoviesContextProvider } from "context/MoviesContext";
+
+import { Routes } from "routes";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
@@ -15,7 +13,6 @@ import {
   faStar as fasFaStar,
 } from "@fortawesome/free-solid-svg-icons";
 import { faStar as farFaStar } from "@fortawesome/free-regular-svg-icons";
-
 library.add(fab, faSearch, faArrowLeft, farFaStar, fasFaStar);
 
 function App() {
@@ -24,9 +21,9 @@ function App() {
       <BrowserRouter>
         <MoviesContextProvider>
           <Switch>
-            <Route component={Home} path="/" exact />
-            <Route component={Detail} path="/detail/:movieId" />
-            <Route component={NotFound} />
+            {Routes.map((route, index) => (
+              <Route key={index} {...route} />
+            ))}
           </Switch>
         </MoviesContextProvider>
       </BrowserRouter>
---
+++ src/routes/index.js
@@ -0,0 +1,18 @@
+import Detail from "pages/Detail";
+import Home from "pages/Home";
+import NotFound from "pages/NotFound";
+
+export const Routes = [
+  {
+    component: Home,
+    path: "/",
+    exact: true,
+  },
+  {
+    component: Detail,
+    path: "/detail/:movieId",
+  },
+  {
+    component: NotFound,
+  },
+];
