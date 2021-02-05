76778457690d722f81538e4ddb4c54d44c56f118
Héctor González <itshgonzalez@gmail.com>
2021-02-05 22:11:32 +0100

    Change API request to https

--- src/services/getMovies.js
+++ src/services/getMovies.js
@@ -1,4 +1,4 @@
 export async function getMovies(movie) {
-  const res = await fetch(`http://www.omdbapi.com/?apikey=${process.env.REACT_APP_APIKEY}&s=${movie}&type=movie`);
+  const res = await fetch(`https://www.omdbapi.com/?apikey=${process.env.REACT_APP_APIKEY}&s=${movie}&type=movie`);
   return await res.json();
 }
