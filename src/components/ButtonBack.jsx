cf93cf481c53e95e81402bfd3f67620edd6a2f51
Héctor González <itshgonzalez@gmail.com>
2022-03-16 11:42:18 +0100

    :recycle: Refactor ButtonBack component

---
+++ src/components/ButtonBack.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { ArrowLeft } from 'iconoir-react';
+
+const SectionHead = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+`;
+
+const TitleHead = styled.h1`
+    font-size: xx-large;
+    margin-left: 1rem;
+`;
+
+const ButtonBck = styled.button`
+    padding: 1rem;
+    background: transparent;
+    border: none;
+    cursor: pointer;
+`;
+
+function ButtonBack({ title }) {
+    return (
+        <SectionHead>
+            <ButtonBck onClick={() => window.history.back()}>
+                <ArrowLeft width={42} height={42} />
+            </ButtonBck>
+            {title ? <TitleHead>{title}</TitleHead> : null}
+        </SectionHead>
+    );
+}
+
+export { ButtonBack };
